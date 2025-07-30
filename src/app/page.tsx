69df78855b809328063836f6b036e675c1a994bf
Bubble Growth <bubbleg.acesso@gmail.com>
2025-07-30 04:42:52 +0000

    deixe a home parecido com a pagina do programa no sentido de design e co

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-background text-foreground font-body antialiased">
       <Header />
-      <main>
+      <main className="flex-grow">
         <Hero />
         <Features />
       </main>
