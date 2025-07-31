ed38590c2bc612ab7ddcbc8ed71827ad9ced35ff
Bubble Growth <bubbleg.acesso@gmail.com>
2025-07-31 06:01:18 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'serpapi.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
